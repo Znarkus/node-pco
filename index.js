3efd3a83d81a49a3865813992b9b56cda3eec19d
Markus Hedlund <znarkus@gmail.com>
2015-05-02 15:51:15 +0200

    Remember url after login, handle multiple service times

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var passport = require('passport');
 var OAuthStrategy = require('passport-oauth').OAuthStrategy;
-var request = require('request');
 var moment = require('moment');
 var OAuth   = require('oauth-1.0a');
 var debug = require('debug')('app');
-var Promise = require('bluebird');
 var util = require('util');
 var config = require('./config.json');
 var oauthConfig = config.oauth;
@@ -21,11 +19,11 @@ var oauthClient = OAuth({
 	},
 	signature_method: 'HMAC-SHA1'
 });
-var people = {};
+var Lib = require('./lib')(oauthClient);
 
 passport.use('provider', new OAuthStrategy(oauthConfig,
 	function(token, tokenSecret, profile, done) {
-		console.log(arguments);
+		//console.log(arguments);
 		done(null, {
 			public: token,
 			secret: tokenSecret
@@ -68,47 +66,11 @@ hbs.registerHelper('format_date', function(date) {
 		res.redirect('/users/' + req.user.username);
 	});*/
 
-function auth(req, res, next) {
-	if (req.user) {
-		next();
-	} else {
-		//req.session.authReturnUrl = req.originalUrl;
-		res.redirect('/auth/provider');
-	}
-}
 
-function callApi(user, method, url, cb) {
-	var request_data = {
-		url: url,
-		method: method
-		//data: {
-		//	status: 'Hello Ladies + Gentlemen, a signed OAuth request!'
-		//}
-	};
-
-	var token = {
-		public: user.public,
-		secret: user.secret
-	};
-
-	return new Promise(function(resolve) {
-		request({
-			url: request_data.url,
-			method: request_data.method,
-			form: oauthClient.authorize(request_data, token)
-		}, function(error, response, body) {
-			debug(body);
-			body = JSON.parse(body);
-			//console.log(body);
-			resolve(body);
-		});
-	});
-}
-
-app.get('/', auth, function (req, res) {
+app.get('/', Lib.auth, function (req, res) {
 	var items = [];
 
-	callApi(req.user, 'GET', 'https://services.planningcenteronline.com/organization.json').then(function(response) {
+	Lib.callApi(req.user, 'GET', 'https://services.planningcenteronline.com/organization.json').then(function(response) {
 
 		recurse(response, 0);
 
@@ -131,63 +93,21 @@ app.get('/', auth, function (req, res) {
 	});
 });
 
-//function loadPeople(req, res, next) {
-//	if (people) {
-//		next();
-//	} else {
-//		callApi(req.user, 'GET', 'https://services.planningcenteronline.com/people.json', function(response) {
-//			people = {};
-//
-//			_.forEach(response.people, function(person) {
-//				people[person.id] = person;
-//			});
-//
-//			next();
-//		});
-//	}
-//}
-
-function fillListWithPersonData(user, items) {
-	return load(0);
-
-	function load(index) {
-		return loadPerson(user, items[index].person_id).then(function(person) {
-			items[index].person = person;
-
-			if (index + 1 < items.length) {
-				return load(index + 1);
-			}
-		})
-	}
-
-}
-
-function loadPerson(user, id) {
-	if (people[id]) {
-		return Promise.resolve(people[id]);
-	} else {
-		return callApi(user, 'GET', 'https://services.planningcenteronline.com/people/' + id + '.json').then(function(response) {
-			people[id] = response;
-			return response;
-		});
-	}
-}
-
-app.get('/:typeId(\\d+)', auth, function(req, res) {
-	callApi(req.user, 'GET', 'https://services.planningcenteronline.com/service_types/' + req.params.typeId + '/plans.json').then(function(response) {
+app.get('/:typeId(\\d+)', Lib.auth, function(req, res) {
+	Lib.callApi(req.user, 'GET', 'https://services.planningcenteronline.com/service_types/' + req.params.typeId + '/plans.json').then(function(response) {
 		res.render('plans', { items: response });
 	});
 });
 
-app.get('/:typeId(\\d+)/:planId(\\d+)', auth, function(req, res) {
-	callApi(req.user, 'GET', 'https://services.planningcenteronline.com/plans/' + req.params.planId + '.json').then(function(response) {
-		return [response, fillListWithPersonData(req.user, response.plan_people)];
+app.get('/:typeId(\\d+)/:planId(\\d+)', Lib.auth, function(req, res) {
+	Lib.callApi(req.user, 'GET', 'https://services.planningcenteronline.com/plans/' + req.params.planId + '.json').then(function(response) {
+		return [response, Lib.fillListWithPersonData(req.user, response.plan_people)];
 	}).spread(function(response) {
-		res.render('plan', { data: response, people: people });
+		res.render('plan', { data: response });
 	});
 });
 
-app.get('/sunday', auth, function(req, res) {
+app.get('/sunday', Lib.auth, function(req, res) {
 	var sundayDate = moment().day(7);
 	var services = [];
 	var filter = {
@@ -196,14 +116,14 @@ app.get('/sunday', auth, function(req, res) {
 		excludePositions: req.query.excludePosition ? req.query.excludePosition.toLowerCase().split(',') : []
 	};
 
-	promisesForEachParallel(filter.serviceTypes, function(serviceType) {
-		return callApi(req.user, 'GET', 'https://services.planningcenteronline.com/service_types/' + serviceType + '/plans.json').then(function(response) {
-			return promisesForEachParallel(response, function(service) {
+	Lib.promisesForEachParallel(filter.serviceTypes, function(serviceType) {
+		return Lib.callApi(req.user, 'GET', 'https://services.planningcenteronline.com/service_types/' + serviceType + '/plans.json').then(function(response) {
+			return Lib.promisesForEachParallel(response, function(service) {
 				//var date = new Date(service.sort_date);
 				if (sundayDate.isSame(service.sort_date, 'day')) {
-					return callApi(req.user, 'GET', 'https://services.planningcenteronline.com/plans/' + service.id + '.json').then(function(response) {
+					return Lib.callApi(req.user, 'GET', 'https://services.planningcenteronline.com/plans/' + service.id + '.json').then(function(response) {
 						services.push(response);
-						//fillListWithPersonData(req.user, response.plan_people, function() {
+						//Lib.fillListWithPersonData(req.user, response.plan_people, function() {
 						//	res.render('plan', { data: response, people: people });
 						//});
 					});
@@ -211,10 +131,10 @@ app.get('/sunday', auth, function(req, res) {
 			});
 		})
 	}).then(function() {
-		return promisesForEachParallel(services, function(service) {
+		return Lib.promisesForEachParallel(services, function(service) {
 			service.filteredPeople = [];
 
-			return promisesForEachParallel(service.plan_people, function(person) {
+			return Lib.promisesForEachParallel(service.plan_people, function(person) {
 				if (_.indexOf(filter.categoryNames, person.category_name.toLowerCase()) != -1) {
 					if (_.indexOf(filter.excludePositions, person.position.toLowerCase()) == -1) {
 						if (person.status != 'D') {
@@ -225,23 +145,28 @@ app.get('/sunday', auth, function(req, res) {
 			});
 		});
 	}).then(function() {
-		return promisesForEachParallel(services, function(service) {
-			return fillListWithPersonData(req.user, service.filteredPeople);
+		return Lib.promisesForEachParallel(services, function(service) {
+			return Lib.fillListWithPersonData(req.user, service.filteredPeople);
 		});
 	}).then(function() {
-		res.render('sunday', { services: services, sundayDate: sundayDate });
+		var filteredEmails = [];
+		//var missingEmail = [];
+
+		_.forEach(services, function (service) {
+
+			_.forEach(service.filteredPeople, function (person) {
+				person.display_service_time = person.position_display_times && service.service_times.length > 1;
+
+				// Extract emails
+				_.forEach(person.person.contact_data.email_addresses, function (email) {
+					filteredEmails.push(email.address);
+				});
+			});
+		});
+
+		res.render('sunday', { services: services, sundayDate: sundayDate, filteredEmails: _.uniq(filteredEmails) });
 	});
 });
-
-function promisesForEachParallel(items, iteratorCb) {
-	var promises = [];
-
-	_.forEach(items, function(item, key) {
-		promises.push(iteratorCb(item, key));
-	});
-
-	return Promise.all(promises);
-}
 
 // Redirect the user to the OAuth provider for authentication.  When
 // complete, the provider will redirect the user back to the application at
@@ -252,21 +177,24 @@ app.get('/auth/provider', passport.authenticate('provider'));
 // Finish the authentication process by attempting to obtain an access
 // token.  If authorization was granted, the user will be logged in.
 // Otherwise, authentication has failed.
-//app.get('/auth/provider/callback', function(req, res, next) {
-//	passport.authenticate('provider', function(err, user, info) {
-//		if (err) { return next(err); }
-//		if (!user) { return res.redirect('/auth/provider'); }
-//		req.logIn(user, function(err) {
-//			if (err) { return next(err); }
-//			return res.redirect(req.session.authReturnUrl ? req.session.authReturnUrl : '/');
-//		});
-//	})(req, res, next);
-//});
-
-app.get('/auth/provider/callback',
-	passport.authenticate('provider', { successRedirect: '/',
-		failureRedirect: '/login' })
-);
+app.get('/auth/provider/callback', function(req, res, next) {
+	passport.authenticate('provider', function(err, user, info) {
+		if (err) { return next(err); }
+		if (!user) { return res.redirect('/auth/provider'); }
+		req.logIn(user, function(err) {
+			//console.error(arguments);
+			//console.error(req.session.authReturnUrl);
+			//process.exit(1);
+			if (err) { return next(err); }
+			return res.redirect(req.session.authReturnUrl ? req.session.authReturnUrl : '/');
+		});
+	})(req, res, next);
+});
+//
+//app.get('/auth/provider/callback',
+//	passport.authenticate('provider', { successRedirect: '/',
+//		failureRedirect: '/login' })
+//);
 
 var server = app.listen(config.port, function () {
 
---
+++ lib.js
@@ -0,0 +1,93 @@
+"use strict";
+
+var Promise = require('bluebird');
+var request = require('request');
+var _ = require('lodash');
+var debug = require('debug')('lib');
+var people = {};
+var oauthClient;
+
+module.exports = function (_oauthClient) {
+	oauthClient = _oauthClient;
+
+	return {
+		fillListWithPersonData: fillListWithPersonData,
+		loadPerson: loadPerson,
+		callApi: callApi,
+		auth: auth,
+		promisesForEachParallel: promisesForEachParallel
+	};
+};
+
+function fillListWithPersonData(user, items) {
+	return load(0);
+
+	function load(index) {
+		return loadPerson(user, items[index].person_id).then(function(person) {
+			items[index].person = person;
+
+			if (index + 1 < items.length) {
+				return load(index + 1);
+			}
+		})
+	}
+
+}
+
+function loadPerson(user, id) {
+	if (people[id]) {
+		return Promise.resolve(people[id]);
+	} else {
+		return callApi(user, 'GET', 'https://services.planningcenteronline.com/people/' + id + '.json').then(function(response) {
+			people[id] = response;
+			return response;
+		});
+	}
+}
+
+function callApi(user, method, url, cb) {
+	var request_data = {
+		url: url,
+		method: method
+		//data: {
+		//	status: 'Hello Ladies + Gentlemen, a signed OAuth request!'
+		//}
+	};
+
+	var token = {
+		public: user.public,
+		secret: user.secret
+	};
+
+	return new Promise(function(resolve) {
+		request({
+			url: request_data.url,
+			method: request_data.method,
+			form: oauthClient.authorize(request_data, token)
+		}, function(error, response, body) {
+			debug(body);
+			body = JSON.parse(body);
+			//console.log(body);
+			resolve(body);
+		});
+	});
+}
+
+function auth(req, res, next) {
+	if (req.user) {
+		next();
+	} else {
+		req.session.authReturnUrl = req.originalUrl;
+		res.redirect('/auth/provider');
+	}
+}
+
+function promisesForEachParallel(items, iteratorCb) {
+	var promises = [];
+
+	_.forEach(items, function(item, key) {
+		promises.push(iteratorCb(item, key));
+	});
+
+	return Promise.all(promises);
+}
